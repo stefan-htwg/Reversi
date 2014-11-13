967beda42a7b84f959e8a14acd3b194ff1065a18
Stefan T <stefan@js-t.de>
2014-11-13 20:31:20 +0100

    test modified

--- src/com/htwg/tests/ReversiModelTests.scala
+++ src/com/htwg/tests/ReversiModelTests.scala
@@ -1,27 +1,35 @@
 package com.htwg.tests
 
-import org.junit.Test
 import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
 import com.htwg.ReversiModel
 
 class ReversiModelTests {
-  
-  @Test def testDefaultValues={
-    
-    var model = new ReversiModel()
 
-    Assert.assertSame(model.getPlayer, 1)
+  val model = new ReversiModel()
+  var startPlayer = 1;
+
+  @Before def initialize() {
   }
-  
-  
-    @Test def testFirstMove={
-    
-    val model = new ReversiModel()
+
+  @Test def testDefaultValues = {
+    Assert.assertSame(model.getPlayer, startPlayer)
+  }
+
+  @Test def testFirstMove = {
+
     val player = model.getPlayer
-    
+    val nextPlayer = if (player == 1) 2 else 1
+
+    Assert.assertSame(model.getAt(4, 5), nextPlayer)
+
     model.clickat(3, 5);
-    
-    Assert.assertSame(model.getAt(3,5), player)
+
+    Assert.assertSame(model.getAt(3, 5), player)
+
+    Assert.assertSame(model.getAt(4, 5), player)
   }
-  
+
 }
