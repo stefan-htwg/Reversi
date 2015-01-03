e03d5494c8e723af64da3ece740c03caa9cadeb7
Michael Kellermeier <Michael.Kellermeier@htwg-konstanz.de>
2015-01-03 17:43:01 +0100

    Test with .project and .classpath file

--- src/com/htwg/ReversiGui.scala
+++ src/com/htwg/ReversiGui.scala
@@ -9,6 +9,7 @@ import scala.swing.Label
 import scala.swing.MainFrame
 import scala.swing.SimpleSwingApplication
 import java.awt.{ Color }
+import javax.swing.JFrame
 
 //TODO show game state running / finished
 
@@ -18,7 +19,7 @@ class ReversiGui(controller: Controller) extends SimpleSwingApplication {
   val height = 8
 
   listenTo(controller)
-
+  
   var buttons: List[Tuple2[Position, Button]] = List()
   var currentPlayerLabel = new Label(controller.getCurrentPlayer.toString)
   var player1Score = new Label(controller.getPlayer1Score.toString)
@@ -34,33 +35,9 @@ class ReversiGui(controller: Controller) extends SimpleSwingApplication {
     title = "Reversi"
     contents =
       new BorderPanel {
-        add(new FlowPanel {
-          contents += new Label("The game is: ")
-          contents += gameStatusLabel
-          contents += new Label("Current player is: ")
-          contents += currentPlayerLabel
-          contents += new Label("Player 1 score: ")
-          contents += player1Score
-          contents += new Label("Player 2 score: ")
-          contents += player2Score
-        }, BorderPanel.Position.North)
-        add(
-          new GridBagPanel {
-            val gbc = new Constraints()
-
-            for (y <- 1 to width) {
-              for (x <- 1 to height) {
-                gbc.gridx = x
-                gbc.gridy = y
-                var button = new Button(Action(controller.getValueAt(x, y).toString) {
-                  println("Try setting cell at: X: " + x + " Y: " + y)
-                  controller.setCell(x, y)
-                })
-                buttons = Tuple2[Position, Button](new Position(x, y), button) :: buttons
-                add(button, gbc)
-              }
-            }
-          }, BorderPanel.Position.Center)
+        add(createHeaderArea, BorderPanel.Position.North)
+        add(createScoreArea, BorderPanel.Position.East)
+        add(createPlayingField, BorderPanel.Position.Center)
       }
     updateUI
   }
@@ -78,6 +55,69 @@ class ReversiGui(controller: Controller) extends SimpleSwingApplication {
     player2Score.text = controller.getPlayer2Score.toString
     gameStatusLabel.text = controller.getGameStatus.toString
     currentPlayerLabel.repaint
+  }
+
+  def createHeaderArea(): FlowPanel = {
+    return new FlowPanel {
+      contents += new Label("The game is: ")
+      contents += gameStatusLabel
+      contents += new Label("Current player is: ")
+      contents += currentPlayerLabel
+      contents += new Button(Action("Game Over") {
+        var newFrame = new JFrame("Game Over") {
+          contents += new Label("The Winner is: Player 1")
+        }
+        newFrame.getContentPane().add(new java.awt.Label("The game is: "), java.awt.BorderLayout.CENTER);
+        newFrame.pack();
+        newFrame.setVisible(true);
+      })
+    }
+  }
+
+  def createPlayingField(): GridBagPanel = {
+    return new GridBagPanel {
+      val gbc = new Constraints()
+
+      for (y <- 1 to width) {
+        for (x <- 1 to height) {
+          gbc.gridx = x
+          gbc.gridy = y
+          var button = new Button(Action(controller.getValueAt(x, y).toString) {
+            println("Try setting cell at: X: " + x + " Y: " + y)
+            controller.setCell(x, y)
+          })
+          buttons = Tuple2[Position, Button](new Position(x, y), button) :: buttons
+          add(button, gbc)
+        }
+      }
+    }
+  }
+
+  def createScoreArea(): GridBagPanel = {
+    return new GridBagPanel {
+      val gbc = new Constraints()
+
+      gbc.gridx = 0
+      gbc.gridy = 0
+      add(new Label("Current score: "), gbc)
+
+      gbc.gridx = 0
+      gbc.gridy = 1
+      add(new Label("Player 1: "), gbc)
+
+      gbc.gridx = 0
+      gbc.gridy = 2
+      add(player1Score, gbc)
+
+      gbc.gridx = 0
+      gbc.gridy = 3
+      add(new Label("Player 2: "), gbc)
+
+      gbc.gridx = 0
+      gbc.gridy = 4
+      add(player2Score, gbc)
+
+    }
   }
 
   def getPlayerColor(cellValue: Integer) = if (cellValue == Player.Player1) Color.red else if (cellValue == Player.Player2) Color.blue else Color.black
--- src/com/htwg/tests/ReversiModelSpecifications.scala
+++
@@ -1,7 +0,0 @@
-package com.htwg.tests
-
-import org.specs2.mutable._
-
-class ReversiModelSpecification extends Specification {
-
-}
