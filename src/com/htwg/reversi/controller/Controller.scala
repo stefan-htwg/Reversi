39b965962b16a1234dddbbef3fdc18fe38045d37
Michael Kellermeier <Michael.Kellermeier@htwg-konstanz.de>
2015-01-11 18:53:40 +0100

    refactored controller

--- src/com/htwg/reversi/controller/Controller.scala
+++ src/com/htwg/reversi/controller/Controller.scala
@@ -10,32 +10,35 @@ import com.htwg.reversi.model.Player;
 case class GameStateChanged() extends Event
 case class BoardChanged() extends Event
 
-class Controller(var model: GameEngine) extends Publisher {
+class Controller(var engine: GameEngine) extends Publisher {
 
   def reset(col: Int, row: Int, startWithPlayer: Integer) {
-   model.reset(new Size(col, row), startWithPlayer)
+   engine.reset(new Size(col, row), startWithPlayer)
    publish(new BoardChanged)
   } 
   
   def reset = {
-   model.reset
+   engine.reset
    publish(new BoardChanged)
   }
   
   def setValueAt(position: Position) {
-      model.doMoveAt(position)
+      engine.doMoveAt(position)
       publish(new GameStateChanged)
     }
 
-  def getValueAt(position: Position) = model.getCellValue(position)
+  def getValueAt(position: Position) = engine.getCellValue(position)
   
-  def getCurrentPlayer = model getPlayer
+  def getCurrentPlayer = engine getPlayer
 
-  def getPlayer1Score = model getScoreFor(Player One)
+  def getPlayer1Score = engine getScoreFor(Player One)
 
-  def getPlayer2Score = model getScoreFor(Player Two)
+  def getPlayer2Score = engine getScoreFor(Player Two)
   
-  def getGameStatus = model getGameStatus
+  def getGameStatus = engine getGameStatus
 
-  override def toString = model toString
+  override def toString = engine toString
 }
+
+
+
